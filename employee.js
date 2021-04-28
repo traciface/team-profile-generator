52a60a52a2ad069dcd3bb7f992e159f13685337c
Traci <traci.face@gmail.com>
2021-04-28 19:57:50 -0400

    first

---
+++ employee.js
@@ -0,0 +1,8 @@
+class employee {
+    constructor (title, employeeName, ID, email) {
+        this.title = title
+        this.employeeName = employeeName
+        this.ID = ID
+        this.email = email
+    }
+}
